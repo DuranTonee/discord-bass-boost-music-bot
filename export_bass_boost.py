fdf34a3e454ea8ccdc55eb77767d9d7b82f5470d
Damir <durantoneeet22@gmail.com>
2023-11-23 17:59:50 +0600

    changed bass boost logic

--- export_bass_boost.py
+++ export_bass_boost.py
@@ -2,24 +2,12 @@ from pydub import AudioSegment
 import numpy as np
 import math
 
-def apply_bass_boost(sample_track, est_mean, est_std):
-    bass_factor = int(round((est_std - est_mean) * 0.010))
-    print(bass_factor)
-
-    boosted_track = sample_track.low_pass_filter(bass_factor)
-
-    return boosted_track
-
 def export_audio_mega(file_path):
     # Load the input audio sample
     sample = AudioSegment.from_file(f"downloads/{file_path}.wav")
     sample_track = np.array(sample.get_array_of_samples())
     
-    est_mean = np.mean(sample_track)
-    est_std = 3 * np.std(sample_track) / (math.sqrt(2))
-
-    # Apply the bass boost effect
-    boosted_sample = apply_bass_boost(sample, est_mean, est_std)
+    boosted_sample = sample_track.low_pass_filter(400)
 
     # Adjust volume and overlay boosted track
     adjusted_sample = sample - 8
