71a22bc1ebcd6767ee6729c5fda5bc85aa731249
Damir <durantoneeet22@gmail.com>
2023-11-23 18:05:31 +0600

    changed value 400 -> 250

--- export_bass_boost.py
+++ export_bass_boost.py
@@ -4,7 +4,7 @@ def export_audio_mega(file_path):
     # Load the input audio sample
     sample = AudioSegment.from_file(f"downloads/{file_path}.wav")
     
-    boosted_sample = sample.low_pass_filter(400)
+    boosted_sample = sample.low_pass_filter(250)
 
     # Adjust volume and overlay boosted track
     adjusted_sample = sample - 8
